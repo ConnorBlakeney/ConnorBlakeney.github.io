9546f87b0ee7e6bd9fc85d3696a1ed2ef5298fdc
Connor Blakeney <connorblakeney@yahoo.com>
2021-01-21 10:53:09 -0600

    fleshing out personal site. added personal section, working on skills section

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,29 @@ $(document).ready(function() {
     });
 
     let typed = new Typed(".sub", {
-        strings: ["Software Engineer.", "Web Developer.", "Student."],
+        strings: ["Software Engineer.", "Web Developer.", "Lifelong Learner."],
         typeSpeed: 70,
         loop: true
     })
 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+    $('.chart').easyPieChart({
+            //your options goes here
+        });
 })
