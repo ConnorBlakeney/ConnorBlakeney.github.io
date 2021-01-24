073df65f4a57ad2c9cdc815fc9f799c67816ca97
Connor Blakeney <connorblakeney@yahoo.com>
2021-01-24 17:50:18 -0600

    finishing up outline for portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -68,5 +68,14 @@ $(document).ready(function() {
         }
     })
 
+    $("[data-fancybox]").fancybox()
 
+    $(".items").isotope({
+        filter: "*",
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    })
 })
