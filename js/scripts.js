6ae92883f2238cb22350e1839e220457055c8a48
Connor Blakeney <connorblakeney@yahoo.com>
2021-01-24 18:24:26 -0600

    added copyright section

--- js/scripts.js
+++ js/scripts.js
@@ -78,4 +78,23 @@ $(document).ready(function() {
             queue: false
         }
     })
+
+    $("#filters a").click(function() {
+
+        $("#filters .current").removeClass("current")
+        $(this).addClass("current")
+
+        let selector = $(this).attr("data-filter")
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        })
+        return false
+
+    })
 })
