0c4377e82d96299246d7fb87447d6c40dcd90bf6
Connor Blakeney <connorblakeney@yahoo.com>
2021-08-09 16:17:36 -0500

    removed stats and skills section for now. added space invaders, plan to add video soon

--- js/scripts.js
+++ js/scripts.js
@@ -27,61 +27,63 @@ $(document).ready(function() {
         loop: true
     })
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        items: 4,
-        responsive:{
-            0:{
-                items:1
-            },
-            480:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            938:{
-                items:4
-            }
-        }
-    })
+// May use commented out code when credentials are better. leaving in for now
+
+    // $('.owl-carousel').owlCarousel({
+    //     loop:true,
+    //     items: 4,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         480:{
+    //             items:2
+    //         },
+    //         768:{
+    //             items:3
+    //         },
+    //         938:{
+    //             items:4
+    //         }
+    //     }
+    // })
 
 
-    let skillsTopOffset = $(".skillsSection").offset().top;
-    let statsTopOffset = $(".statsSection").offset().top;
-    let countUpFinished = false
+    // let skillsTopOffset = $(".skillsSection").offset().top;
+    // let statsTopOffset = $(".statsSection").offset().top;
+    // let countUpFinished = false
 
-    $(window).scroll(function() {
-        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+    // $(window).scroll(function() {
+    //     if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
 
-            $('.chart').easyPieChart({
-                    easing: 'easeInOut',
-                    barColor: 'white',
-                    trackColor: false,
-                    scaleColor: false,
-                    lineWidth: 4,
-                    size: 152,
-                    onStep: function(from, to, percent) {
-                        $(this.el).find('.percent').text(Math.round(percent))
-                    }
-                })
+    //         $('.chart').easyPieChart({
+    //                 easing: 'easeInOut',
+    //                 barColor: 'white',
+    //                 trackColor: false,
+    //                 scaleColor: false,
+    //                 lineWidth: 4,
+    //                 size: 152,
+    //                 onStep: function(from, to, percent) {
+    //                     $(this.el).find('.percent').text(Math.round(percent))
+    //                 }
+    //             })
 
-        }
-    })
+    //     }
+    // })
 
-    $(window).scroll(function() {
-        if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+    // $(window).scroll(function() {
+    //     if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
 
             
-            $(".counter").each(function() {
-                let element = $(this)
-                let endVal = parseInt(element.text())
+    //         $(".counter").each(function() {
+    //             let element = $(this)
+    //             let endVal = parseInt(element.text())
 
-                element.countup(endVal)
-            })
-            countUpFinished = true
-        }
-    })
+    //             element.countup(endVal)
+    //         })
+    //         countUpFinished = true
+    //     }
+    // })
 
     $("[data-fancybox]").fancybox()
 
